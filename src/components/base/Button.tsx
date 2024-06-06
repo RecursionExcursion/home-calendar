c72c9e625d959fe072e81aabfd330cfbff000e7b
RecursionExcursion <rloup15@gmail.com>
2024-06-06 16:02:29 -0400

    changed button to normal css

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,19 +1,28 @@
-import { ComponentPropsWithoutRef } from "react";
+import { CSSProperties, ComponentPropsWithoutRef } from "react";
 
 type CustomButtonProps = ComponentPropsWithoutRef<"button"> & {
   text: string;
   theme?: keyof typeof buttonStyles;
 };
 
-const buttonStyles = {
-  primary:
-    "border border-white px-2 py-1 rounded-md min-w-24 flex justify-center text-white bg-black text-nowrap",
+const buttonStyles: Record<string, React.CSSProperties> = {
+  primary: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+    color: "#FFFFFF",
+    minWidth: "6rem",
+    border: "1px solid #FFFFFF",
+    borderRadius: "0.25rem",
+    padding: "0.25rem .5rem",
+    textWrap: "nowrap",
+  },
 };
 
 export default function Button(props: CustomButtonProps) {
   const { text, theme = "primary", ...attr } = props;
   return (
-    <button className={buttonStyles[theme]} {...attr}>
+    <button {...attr} style={buttonStyles[theme]}>
       {props.text}
     </button>
   );
