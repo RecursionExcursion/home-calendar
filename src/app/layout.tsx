80be3f17fc75ec100e0524c93dd2aa3b9e557d77
RecursionExcursion <rloup15@gmail.com>
2024-06-11 17:10:03 -0400

    updated gitignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "../contexts/AppContext";
 import { Analytics } from "@vercel/analytics/react";
-import { update } from "../../.vsscripts/dbUpdate";
 
 const inter = Inter({ subsets: ["latin"] });
 
