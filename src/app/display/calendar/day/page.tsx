eb445f59f7583e23ebd5f1310a06c1b415f37532
RecursionExcursion <rloup15@gmail.com>
2024-06-19 21:22:27 -0400

    removed flex start

--- src/app/display/calendar/day/page.tsx
+++ src/app/display/calendar/day/page.tsx
@@ -3,6 +3,7 @@
 import Calendar from "../../../../components/display/calendar/Calendar";
 
 export default async function CalendarDayView() {
+  //TODO remove flex start
   return (
     <div className="full flex" style={{ alignItems: "flex-start" }}>
       <Calendar mode={"day"} />
