bfd93f27c90cfdb061f5bd3a92a2dbd767c96e5f
RecursionExcursion <rloup15@gmail.com>
2024-06-27 15:27:01 -0400

    Added a 500ms to default app loading time fore less flashing

--- src/hooks/useAppLoading.tsx
+++ src/hooks/useAppLoading.tsx
@@ -5,7 +5,7 @@ import { useAppLoadingContext } from "../contexts/LoadingContext";
 
 export default function useAppLoading(toState?: boolean, msDelay?: number) {
   const loadingState = toState ?? false;
-  const delay = msDelay ?? 0;
+  const delay = msDelay ?? 500;
 
   const { setAppLoading } = useAppLoadingContext();
 
