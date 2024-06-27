fe3b9fb3b9309c876d965399be6b5784fbef910e
RecursionExcursion <rloup15@gmail.com>
2024-06-27 09:29:55 -0400

    clean up

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,6 @@
-import DashboardSideBar from "../../components/dashboard/menu/DashboardSideBar";
 import { DashboardProvider } from "../../contexts";
 import { UserContentProvider } from "../../contexts/UserContentProvider";
 import { LoadingProvider } from "../../contexts/LoadingContext";
-import { UserProvider } from "../../contexts/UserContext";
 import DashboardSlideOutMenu from "../../components/dashboard/menu/DashboardSlideOutMenu";
 
 type DashboardLayoutProps = {
@@ -14,7 +12,6 @@ export default async function DashboardLayout(props: DashboardLayoutProps) {
   return (
     <UserContentProvider>
       <div className="db-layout-container">
-        {/* <DashboardSideBar /> */}
         <DashboardSlideOutMenu />
         <div className="full flex relative">
           <DashboardProvider>
